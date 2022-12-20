1eea3db8054d6d424d743077d90efe325a7b5921
evandrotvc <evandro-tvc@hotmail.com>
2022-12-20 10:47:24 -0300

    fix: ajusta texto

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -10,17 +10,17 @@ const Home: React.FC = ({}) => {
 
         <Card author= 'J.K Rowling'
             total_ranking={0}
-            description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental; O Mar de Monstros, quando ele e seus amigos se envolvem numa perigosa aventura para defender o Acampamento Meio-Sangue; A maldição do Titã'
+            description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental'
         />
 
         <Card author= 'J.K Rowling'
             total_ranking={0}
-            description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental; O Mar de Monstros, quando ele e seus amigos se envolvem numa perigosa aventura para defender o Acampamento Meio-Sangue; A maldição do Titã'
+            description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental'
         />
 
         <Card author= 'J.K Rowling'
             total_ranking={0}
-            description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental; O Mar de Monstros, quando ele e seus amigos se envolvem numa perigosa aventura para defender o Acampamento Meio-Sangue; A maldição do Titã'
+            description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental'
         />
      </ContainerPage>
     )
