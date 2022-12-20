dfcfa11f6cfbf140e65e3af88102f43fa18968b1
evandrotvc <evandro-tvc@hotmail.com>
2022-12-20 10:39:55 -0300

    feat: adiciona styles de hover no card e 1 style no root do projeto

--- front/src/components/Card/styles.ts
+++ front/src/components/Card/styles.ts
@@ -4,6 +4,8 @@ export const Container = styled.div `
     height: 500px;
     width: 500px;
     background-color: white;
+    margin: 1em;
+    transition: transform .25s ease-out;
 
     img{
         width: 100%;
@@ -13,5 +15,12 @@ export const Container = styled.div `
 
     p{
         color: black;
+    }
+
+    &:hover{
+        box-shadow: 0px -1px 21px 1px rgba(151,206,76,0.79);
+        -webkit-box-shadow: 0px -1px 21px 1px rgba(151,206,76,0.79);
+        -moz-box-shadow: 0px -1px 21px 1px rgba(151,206,76,0.79);         
+        transform: scale(1.05);
     }
 `;
