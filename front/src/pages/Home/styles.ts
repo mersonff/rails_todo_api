ae82556898f95be7499af88b87a60f2e6ce78bc4
evandrotvc <evandro-tvc@hotmail.com>
2022-12-20 12:05:27 -0300

    feat: styles no container page display: flex

--- front/src/pages/Home/styles.ts
+++ front/src/pages/Home/styles.ts
@@ -3,5 +3,9 @@ import styled from 'styled-components'
 export const ContainerPage = styled.div`
     width: 100%;
     height: 100%;
-    display: flex;
+    /* display: flex; */
+
+    display: inline-flex;
+    justify-content: center;
+    flex-flow: wrap;
 `;
