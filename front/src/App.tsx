0156e53fc25048985574f22f87c6de438588f2f6
evandrotvc <evandro-tvc@hotmail.com>
2022-12-20 10:07:22 -0300

    feat: instala mui material e adiciona ranking no component  card

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -9,7 +9,9 @@ function App() {
 
   return (
     <>
-      <Card author= 'J.K Rowling' description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental; O Mar de Monstros, quando ele e seus amigos se envolvem numa perigosa aventura para defender o Acampamento Meio-Sangue; A maldição do Titã'/>
+      <Card author= 'J.K Rowling'
+      total_ranking={0}
+      description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental; O Mar de Monstros, quando ele e seus amigos se envolvem numa perigosa aventura para defender o Acampamento Meio-Sangue; A maldição do Titã'/>
     </>
   )
 }
--- front/src/components/Card/index.tsx
+++ front/src/components/Card/index.tsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import {Container} from './styles'
+import Rating from '@mui/material/Rating'
 
 interface Cardprops {
     url?: string
     author: string
     description: string
+    total_ranking: number
 }
 
-const Card: React.FC<Cardprops> = ({author, description}) => (
+const Card: React.FC<Cardprops> = ({author, description, total_ranking}) => {
+    const [ranking, setRaking] = useState<number | null>(total_ranking)
+
+    return (
     <Container>
         <img src='https://www.rickriordan.com.br/assets/img/percyjackson/gallery/o_ladrao_de_raios_gn.png' alt='teste'></img>
+        <Rating
+            name="simple-controlled"
+            value={ranking}
+            onChange={(event, newValue) => {
+            setRaking(newValue);
+        }}
+        />
         <div>
             <p>{author}</p>
             <p>{description}</p>
         </div>
     </Container>
-);
+)};
 
 export default Card;
