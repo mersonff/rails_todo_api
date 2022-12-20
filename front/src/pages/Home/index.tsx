9cbb453da2cba1cc568550bfb955c7c7288ea028
evandrotvc <evandro-tvc@hotmail.com>
2022-12-20 10:41:33 -0300

    feat: styles iniciais para o home page

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -1,15 +1,28 @@
 import React, { useState } from 'react'
 
+import {ContainerPage} from './styles';
+
 import Card from '../../components/Card';
 
 const Home: React.FC = ({}) => {
     return (
-     <>
+     <ContainerPage>
 
-    <Card author= 'J.K Rowling'
-      total_ranking={0}
-      description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental; O Mar de Monstros, quando ele e seus amigos se envolvem numa perigosa aventura para defender o Acampamento Meio-Sangue; A maldição do Titã'/>
-     </>
+        <Card author= 'J.K Rowling'
+            total_ranking={0}
+            description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental; O Mar de Monstros, quando ele e seus amigos se envolvem numa perigosa aventura para defender o Acampamento Meio-Sangue; A maldição do Titã'
+        />
+
+        <Card author= 'J.K Rowling'
+            total_ranking={0}
+            description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental; O Mar de Monstros, quando ele e seus amigos se envolvem numa perigosa aventura para defender o Acampamento Meio-Sangue; A maldição do Titã'
+        />
+
+        <Card author= 'J.K Rowling'
+            total_ranking={0}
+            description='O ladrão de raios, em que Percy descobre sua ligação com os deuses do Olimpo e precisa impedir uma guerra entre eles, que acabaria com toda a civilização ocidental; O Mar de Monstros, quando ele e seus amigos se envolvem numa perigosa aventura para defender o Acampamento Meio-Sangue; A maldição do Titã'
+        />
+     </ContainerPage>
     )
 };
 
